fed5f4a6b9a85e53447a617580abc6ec9b1d79e0
Sergio Cuesta Barrera <scuesta33@gmail.com>
2025-01-09 09:40:36 +0100

    Cambios en emailService para mejorar los mensajes en los emails

--- src/main/java/com/example/SaludClick/controller/EmailController.java
+++ src/main/java/com/example/SaludClick/controller/EmailController.java
@@ -17,23 +17,7 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    // Endpoint para enviar correo de confirmación
-    @PostMapping("/sendConfirmation")
-    public String sendConfirmationEmail(
-            @RequestParam String toEmail,
-            @RequestParam String concertName,
-            @RequestParam String concertDate,
-            @RequestParam String concertLocation) {
-        
-        try {
-            // Llama al servicio para enviar el correo
-            emailService.sendConfirmationEmail(toEmail, concertName, concertDate, concertLocation);
-            return "Correo de confirmación enviado exitosamente a " + toEmail;
-        } catch (MessagingException e) {
-            // Si ocurre un error al enviar el correo, retorna un mensaje de error
-            return "Error al enviar el correo de confirmación: " + e.getMessage();
-        }
-    }
+   
     @PostMapping("/sendAccountDeletion")
     public String sendAccountDeletionEmail(@RequestParam String toEmail) {
         try {
--- src/main/java/com/example/SaludClick/service/EmailService.java
+++ src/main/java/com/example/SaludClick/service/EmailService.java
@@ -23,7 +23,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Cita Creada");
             helper.setText("<h1>¡Tu cita ha sido creada!</h1>" +
@@ -44,7 +44,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Cita Actualizada");
             helper.setText("<h1>¡Tu cita ha sido actualizada!</h1>" +
@@ -65,7 +65,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Cita Cancelada");
             helper.setText("<h1>Tu cita ha sido cancelada</h1>" +
@@ -79,35 +79,14 @@ public class EmailService {
             throw new MessagingException("Error al enviar el correo de cancelación de cita", e);
         }
     }
-    public void sendConfirmationEmail(String toEmail, String concertName, String concertDate, String concertLocation) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        try {
-            helper.setFrom("scuesta33@gmail.com");
-            helper.setTo(toEmail);
-            helper.setSubject("Confirmación de compra - Entrada para " + concertName);
-            helper.setText("<h1>¡Gracias por tu compra!</h1>" +
-                    "<p>Has comprado una entrada para el concierto: <strong>" + concertName + "</strong></p>" +
-                    "<p><strong>Fecha del concierto:</strong> " + concertDate + "</p>" +
-                    "<p><strong>Ubicación:</strong> " + concertLocation + "</p>" +
-                    "<p>¡Nos vemos en el concierto!</p>", true);
-
-            mailSender.send(message);
-            logger.info("Correo enviado a: " + toEmail);
-
-        } catch (MessagingException e) {
-            logger.error("Error al enviar el correo a " + toEmail, e);
-            throw new MessagingException("Error al enviar el correo de confirmación", e);
-        }
-    }
 
     public void sendAccountDeletionEmail(String toEmail) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Confirmacion de eliminacion de cuenta");
             helper.setText("<h1>Tu cuenta ha sido eliminada</h1>" +
@@ -127,7 +106,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Confirmación de actualización de credenciales");
             helper.setText("<h1>Tus credenciales han sido actualizadas</h1>" +
@@ -147,10 +126,10 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Confimación de registro");
-            helper.setText("<h1>Bienvenido a TicketsApp</h1>" +
+            helper.setText("<h1>Bienvenido a SaludClick</h1>" +
                     "<p>Tu usuario ha sido registrado exitosamente.</p>", true);
 
             mailSender.send(message);
@@ -166,7 +145,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         try {
-            helper.setFrom("scuesta33@gmail.com");
+        	helper.setFrom("SaludClick <scuesta33@gmail.com>");
             helper.setTo(toEmail);
             helper.setSubject("Recordatorio de Cita");
             helper.setText("<h1>Recordatorio de tu cita</h1>" +
